290c360a596c118f221246deeaf9412d89e8863d
Richard Guo <r.guo@aerospike.com>
2017-01-26 16:52:17 -0800

    Removed collectd warning

--- asgraphite.py
+++ asgraphite.py
@@ -259,7 +259,7 @@ class Client(object):
 			except socket.error as msg:
 				s.close()
 				s = None
-				collectd.warning("Connect Error" % msg)
+				print "Connect Error" % msg
 				continue
 			break
 
